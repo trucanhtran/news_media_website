e5296b89f184c4a98d73e4d09384f52686bc0d18
trucanh <anhtructran111@gmail.com>
2021-07-23 18:00:09 +0700

    handle crawl data from zingnews

--- lib/tasks/sites/zingnews.rb
+++ lib/tasks/sites/zingnews.rb
@@ -15,7 +15,12 @@ class ZingNews
 
   def start_parsing
     raw_home_page = using_nokogiri(@url)
-    collect_category(raw_home_page)
+    arr_category = collect_category(raw_home_page)
+
+    arr_category.each do |category|
+      current_category = category[:category_link]
+      get_product_links(current_category)
+    end
   end
 
   def collect_category(raw_home_page)
@@ -30,8 +35,31 @@ class ZingNews
         puts "Error category here #{link}"
       end
     end
-    puts arr_category
     arr_category
+  end
+
+  def get_product_links(current_category)
+    arr_product_list = []
+    document = using_nokogiri(current_category)
+    document.css(".section-content .article-list .article-item").each do |link|
+      product_link = @url + link.css(".article-title a").first['href']
+      product_thumbnail = link.css(".article-thumbnail a img").first['src']
+      arr_product_list << {product_link: product_link, product_thumbnail: product_thumbnail}
+    end
+
+    arr_product_list.each do |link|
+      current_product = link[:product_link]
+      get_product_details(current_product)
+    end
+  end
+
+  def get_product_details(current_product)
+    arr_product_detail = []
+    document = using_nokogiri(current_product)
+    title = document.css(".the-article-header .the-article-title").first.content
+    # author = document.css("ul.the-article-meta li.the-article-author a")
+    description = document.css(".main .the-article-summary").first.content
+    content = document.css(".main .the-article-body").first.content
   end
 
 end
