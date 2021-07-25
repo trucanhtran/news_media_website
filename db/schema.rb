bcacdf693e97ea41df974f7cd8e78ca328d39593
trucanh <anhtructran111@gmail.com>
2021-07-25 19:50:14 +0700

    handle crawl data form zingnews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_145701) do
+ActiveRecord::Schema.define(version: 2021_07_23_145551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2021_07_22_145701) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.text "title"
+    t.text "description"
+    t.text "content"
+    t.boolean "hot_new"
+    t.bigint "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
--- lib/tasks/sites/zingnews.rb
+++ lib/tasks/sites/zingnews.rb
@@ -17,9 +17,13 @@ class ZingNews
     raw_home_page = using_nokogiri(@url)
     arr_category = collect_category(raw_home_page)
 
-    arr_category.each do |category|
-      current_category = category[:category_link]
-      get_product_links(current_category)
+    arr_category.each do |link|
+      begin
+        current_category = link[:category_link]
+        get_product_links(current_category, link[:category])
+      rescue
+        puts "Error: #{link}"
+      end
     end
   end
 
@@ -29,8 +33,8 @@ class ZingNews
       begin
         category_name = link.at_css('a').values.last
         category_link =  @url + link.at_css('a').values.first
-        arr_category << {category_link: category_link, category_name: category_name}
-        Category.find_or_create_by(name: category_name)
+        category = Category.find_or_create_by(name: category_name)
+        arr_category << {category_link: category_link, category_name: category_name, category: category}
       rescue
         puts "Error category here #{link}"
       end
@@ -38,28 +42,46 @@ class ZingNews
     arr_category
   end
 
-  def get_product_links(current_category)
+  def get_product_links(current_category, category)
     arr_product_list = []
     document = using_nokogiri(current_category)
     document.css(".section-content .article-list .article-item").each do |link|
-      product_link = @url + link.css(".article-title a").first['href']
-      product_thumbnail = link.css(".article-thumbnail a img").first['src']
-      arr_product_list << {product_link: product_link, product_thumbnail: product_thumbnail}
+      begin
+        product_link = @url + link.css(".article-title a").first['href']
+        product_thumbnail = link.css(".article-thumbnail a img").first['src']
+        arr_product_list << {product_link: product_link, product_thumbnail: product_thumbnail}
+      rescue
+        puts "Error: #{link}"
+      end
     end
 
     arr_product_list.each do |link|
-      current_product = link[:product_link]
-      get_product_details(current_product)
+      begin
+        current_product = link[:product_link]
+        get_product_details(current_product, category)
+      rescue
+        puts "Error: #{link}"
+      end
     end
   end
 
-  def get_product_details(current_product)
+  def get_product_details(current_product, category)
     arr_product_detail = []
     document = using_nokogiri(current_product)
     title = document.css(".the-article-header .the-article-title").first.content
-    # author = document.css("ul.the-article-meta li.the-article-author a")
+    # author = document.at_css(".the-article-author a")
     description = document.css(".main .the-article-summary").first.content
-    content = document.css(".main .the-article-body").first.content
+    content = document.css(".main .the-article-body").first.inner_html
+    product = Product.find_by(title: title)
+    if product.present?
+      product.description = description
+      product.content = content
+      product.save
+      p product.content
+    else
+      new_product = category.products.create(title: title, description: description, content: content)
+      p new_product.content
+    end
   end
 
 end
