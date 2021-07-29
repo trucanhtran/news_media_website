a7824ec7e19627f488f09dd3dacabff84f88c59a
trucanh <anhtructran111@gmail.com>
2021-07-29 14:43:33 +0700

    creat index view for zingnews

--- app/controllers/zingnews_controller.rb
+++ app/controllers/zingnews_controller.rb
@@ -1,8 +1,25 @@
 class ZingnewsController < ApplicationController
+  before_action :get_data, only: %[index]
+
   def index
-    @categories = Category.all.limit(10)
     @products = Product.all.limit(5)
     @hot_product = Product.all.order(updated_at: :desc).first
     @hot_products = Product.all.order(updated_at: :desc).limit(2).offset(1)
+    @book = Category.find_by(name: 'Sách hay')
+    @book_products = @book.products.all
+    @latest_products = Product.all.order(updated_at: :desc).limit(30)
+
   end
+
+  def show_category
+    @category = Category.find_by(id: params[:id])
+    @products = @category.products.all
+  end
+
+  private
+
+  def get_data
+    @categories = Category.all.limit(10)
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def get_time(product_id)
+    latest_product = Product.find_by(id: params[:product_id])
+    latest_time = latest_product.updated_at.strftime("%H")
+    current_time = Time.now.strftime("%H")
+    time = current_time - latest_time
+    return time
+  end
+
 end
